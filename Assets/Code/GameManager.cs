66ee13cdd8509f571e92bad5ee5ca2885a53cba7
Rohit Moni <rohitmdxb@gmail.com>
2015-05-27 16:41:02 -0700

    Added in the decision panel with lerp sliding

--- Assets/Code/GameManager.cs
+++ Assets/Code/GameManager.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 namespace Assets.Code
 {
@@ -6,21 +7,36 @@ namespace Assets.Code
     {
         // private references set in code
         private SenateManager _senateManager;
+        private GameObject _decisionPanel;
 
         // member variables
+        private bool _decisionPanelActive;
+        private Coroutine _decisionPanelSlideCoroutine;
+
+        // constants
+        private const float PanelSlideTime = 0.5f;
 
         void Awake()
         {
             _senateManager = GetComponent<SenateManager>();
+            _decisionPanel = GameObject.Find("DecisionPanel");
         }
 
         // Use this for initialization
-        void Start () {
+        void Start ()
+        {
+            _decisionPanelActive = false;
+            _decisionPanelSlideCoroutine = null;
         }
 	
         // Update is called once per frame
         void Update () {
-	
+#if UNITY_EDITOR
+            if (Input.GetKeyDown(KeyCode.Tab))
+            {
+                ToggleDecisionPanel();
+            }
+#endif
         }
 
         public void CoupButtonPressed()
@@ -33,6 +49,35 @@ namespace Assets.Code
             {
                 
             }
+        }
+
+        public void ToggleDecisionPanel()
+        {
+            _decisionPanelActive = !_decisionPanelActive;
+
+            if (_decisionPanelSlideCoroutine != null)
+                StopCoroutine(_decisionPanelSlideCoroutine);
+
+            _decisionPanelSlideCoroutine = (_decisionPanelActive ? StartCoroutine(SlideDecisionPanel(_decisionPanel.transform.position.x, Screen.width * 0.5f, PanelSlideTime)) : StartCoroutine(SlideDecisionPanel(_decisionPanel.transform.position.x, Screen.width * 1.5f, PanelSlideTime)));
+        }
+
+        IEnumerator SlideDecisionPanel(float startXPosition, float finalXPosition, float time)
+        {
+            var timer = 0f;
+            var position = _decisionPanel.transform.position;
+            while (timer < time)
+            {
+                timer += Time.smoothDeltaTime;
+
+                position.x = Mathf.Lerp(startXPosition, finalXPosition, Mathf.SmoothStep(0, 1, timer/time));
+                _decisionPanel.transform.position = position;
+                yield return null;
+            }
+
+            position.x = finalXPosition;
+            _decisionPanel.transform.position = position;
+
+            yield return null;
         }
     }
 }
